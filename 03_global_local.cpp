90d9ccf6bd62f181dac58d34702e08b7b9edf5fb
Anamicacodes <anamicagupta246@gmail.com>
2026-07-08 17:07:01 +0530

    Adding changes and comments about results

--- 03_global_local.cpp
+++ 03_global_local.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
-int x;          //global 
+int x=10;          //global 
 int main(){
-    int y;      //local
+    int y =20;      //local
     cout <<x << "\n"; //prints 0
-    cout <<y;         //prints garbage value 
+    cout <<y <<endl;         //prints garbage value 
+    x=40;
+    y = 30;
+    cout << "After changing" <<endl;
+    cout <<x << "\n"; //prints 40 --doesnt change rather assigns a local var x 
+    cout <<y;         //prints 30
     return 0;
 }
