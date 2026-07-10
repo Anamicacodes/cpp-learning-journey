39808be681f92e17a7dee1c8b280a8398570f15c
Anamicacodes <anamicagupta246@gmail.com>
2026-07-10 20:40:03 +0530

    Adding assignment and relational operator

--- 04_operators.cpp
+++ 04_operators.cpp
@@ -34,5 +34,39 @@ int main(){
     cout << "Logical AND : " << num1 << " && " << num2 <<" is: " << (num1&&num2) << endl;
     cout << "Logical OR : " << num1 << "||" << num2 <<" is: "<< (num1||num2) << endl;
     cout << "Logical NOT : !" << num1  << " is: " << (!num1) << endl;
+    //ASSIGNMENT operator: 
+    int a=num1;
+    int b=0;
+    cout << "Simple Assignment : a = num1 then a = " << a<<endl;
+    b +=a;
+    cout << "Add and assign : b+=a : then b = " << b <<endl;
+    b-=a;
+    cout << "Subtract and assign : b-=a then b= " << b <<endl; 
+    b*=a;
+    cout << "Multiply and assign : b*=a then b= " << b <<endl;
+    b/=a;
+    cout << "Divide and assign : b/=a then b= " << b <<endl;
+    b%=a;
+    cout << "Modulus and assign : b%=a then b= " << b <<endl ;
+    b&=a;
+    cout << "BITWISE AND and assign : b&=a then b= " << b <<endl ;
+    b^=a;
+    cout << "Bitwise XOR and assign : b^=a then b= " << b <<endl ;
+    b<<=1;
+    cout << "Left shift and assign : b<<=1 then b= " << b <<endl ;
+    //Find the result : 
+    int x,y,z;
+    x=y=z=10;
+    cout << x <<y <<z <<endl ; // prints 101010
+    //Relational Operators : 
+    a=10;
+    b = 20; 
+    cout << " a= " <<a << " and b= " << b << endl ;
+    cout <<" a==b : " << (a==b) << endl;
+    cout <<" a!=b : " << (a!=b) << endl;
+    cout <<" a>b : " << (a>b) << endl;
+    cout <<" a<b : " << (a<b) << endl;
+    cout <<" a>=b : " << (a>=b) << endl;
+    cout <<" a<=b : " << (a<=b) << endl;
     return 0;
 }
