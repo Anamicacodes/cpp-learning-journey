51a9352299ee4efac3b9818a34c1a9f20899cb57
Anamicacodes <anamicagupta246@gmail.com>
2026-07-11 22:42:06 +0530

    Nested if else 2 programs added

--- 05_if_else.cpp
+++ 05_if_else.cpp
@@ -6,7 +6,7 @@ int main(){
     cout <<"Enter a number :" ;
     cin >> num;
     if (num%2 ==0 )
-        cout <<"Divisible by 2";
+        cout <<"Divisible by 2" <<endl;
 
     //if else  :
     int age;
@@ -17,7 +17,7 @@ int main(){
         cout <<"Please move towards the polling booth!"<<endl;
     }
     else 
-        cout << "Can not vote";
+        cout << "Can not vote" <<endl;
 
     //else if : 
     int marks ;
@@ -35,5 +35,78 @@ int main(){
         cout << "Your ward secure C grade! " <<endl ;
     }
     else
-        cout << "Your ward failed!" <<endl ;
+        cout << "Your ward failed!" <<endl ; 
+
+    //Nested if :
+    int pin;
+    cout << "Enter your pin :" ;
+    cin >> pin;
+    int correct_pin = 2007;
+    int bal = 1000000;
+    int daily_limit = 5000;
+    int amt;
+    if (pin == correct_pin){
+        cout << "Enter amount to be withdrawn : ";
+        cin >> amt;
+        if (amt <= bal){
+            if (amt <= daily_limit){
+                if (amt %100 == 0){
+                    cout << "Please collect your cash!"<<endl;
+                }
+            }
+            else {
+                cout << "Daily Limit exceeded!"<<endl;
+            }
+        } 
+        else {
+            cout << "Insuffiecient balance!"<<endl;
+        }
+    }
+    else {
+        cout << "Incorrect pin!"<<endl;
+    }
+
+    //Nested if else :
+    string stream;
+    cout << "Enter your stream (Sci/comm): ";
+    cin >> stream;
+    string sub;
+    cout << "Enter your subject (PCM/Accounts) :";
+    cin >> sub;
+    int markss;
+    
+    if (stream == "Sci" || stream == "sci" || stream == "Science" || stream == "science"){
+        if (sub == "PCM") {
+            cout << "Enter you marks : ";
+            cin >> markss;
+            if(markss >=90 ){
+                cout << "Enligible for engineering!" << endl;
+            }
+            else {
+                cout << "Not Enligible for engineering!" << endl ;
+            }
+        }
+        else {
+            cout << "Incorrect subject!" <<endl;
+        }
+    }
+    else if (stream == "Comm" || stream =="comm" || stream =="Commerce" || stream == "commerce"){
+        if (sub == "Accounts") {
+            cout << "Enter you marks : ";
+            cin >> markss;
+            if(markss >=90 ){
+                cout << "Enligible for BBA/BCA!" << endl;
+            }
+            else {
+                cout << "Not Enligible for BBA/BCA!" << endl ;
+            }
+        }
+        else {
+            cout << "Incorrect subject!" <<endl;
+        }
+    }
+    else {
+        cout <<"Choose a suitable course!";
+    }
+     
 }
