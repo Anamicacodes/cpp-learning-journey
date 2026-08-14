2ac6231e0816f43ae6abd052a4a56edb83c98b13
Anamicacodes <anamicagupta246@gmail.com>
2026-08-14 13:12:48 +0530

    Call by value and call by reference

--- 11_Pointers.cpp
+++ 11_Pointers.cpp
@@ -8,6 +8,12 @@ void test(int &x, int y){
     cout << x << endl << y << endl;
 }
 
+void pointer_method(int *x, int y){
+    *x +=5;        //pointer call by reference
+    y *=2;         //call by value
+    cout << *x <<y;
+}
+
 int main(){
     int a =15;
     int* p = &a;
@@ -24,4 +30,19 @@ int main(){
     int m=3, n=4;
     test(m,n);
     cout << m <<endl << n << endl;
+
+    int i=3,q=4;
+    pointer_method(&i,q);
+    cout << i << q ;
+    
+    int k = 10;
+    int *l = &k;
+    *l = 25;
+    cout << k;    //pointer value change
+
+    int a = 5;
+    int b = 10;
+    int *p = &a;
+    *p = b;          //is pointing differently now or assigning 
+    cout << a << " " << b;    
 }
--- Class.cpp
+++ Class.cpp
@@ -180,7 +180,8 @@ cout << endl;
 
    for (int i=1;i<=4;i++){
         for (int j=1;j<=7;j++){
-            if((j>=5-i) && (j<= 3+i)){
+            if ((j>=5-i) && (j<= 3+i))
+            {
                 cout<< "*";
             }
             else {
