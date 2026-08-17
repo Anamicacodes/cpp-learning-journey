b6bbf340cf4b3736577b09aceccb07c17c97400e
Anamicacodes <anamicagupta246@gmail.com>
2026-08-17 12:13:42 +0530

    Arrays

---
+++ 12_Arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void PrintArray(int arr[], int size){
+    for(int i=0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+}
+int main(){
+    int numbers[]= {10,20,30,40,50};
+    int size =5;
+    PrintArray(numbers,size);
+}
