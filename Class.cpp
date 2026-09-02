adb8b19218a200f955a95e29122b13984879921b
Anamicacodes <anamicagupta246@gmail.com>
2026-09-02 14:48:10 +0530

    Inline functions and defalut parameters

--- Class.cpp
+++ Class.cpp
@@ -1,37 +1,15 @@
+//Inline Functions : 
+//2nd september
 #include <iostream>
 using namespace std;
-class Parent {
-    public: 
-    int sum(int a,int b){
-        return a+b;
-    }
-};
-class A: public Parent {
-    public: 
-    int diff(int a,int b){
-        return a-b;
-    }
-};
-class B: public Parent {
-    public: 
-    int mul(int a,int b){
-        return a*b;
-    }
-};
-class C: public Parent {
-    public: 
-    int div(int a,int b){
-        return a/b;
-    }
-};
+inline int addition(int a,int b){
+    return a+b;
+}
+int add(int a,int b=5){   //we cant pass default arguments into the first element 
+    return a+b;
+}
 int main(){
-    A a;
-    cout << "Sum from parent of 5 and 3 is : " << a.sum(5,3)<< endl;
-    cout << "Difference of 5 and 3 is : "<< a.diff(5,3)<< endl;
-    B b;
-    cout << "Multiply of 5 and 3 is : " <<b.mul(5,3) << endl;
-    C c;
-    cout << "Division of 10 and 5 is : " <<c.div(10,5) << endl;
-
-
+    int x = addition(10,25);
+    cout << x << endl;
+    cout << add(25);
 }
