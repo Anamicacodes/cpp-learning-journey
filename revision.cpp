5e87feee3d5128405fff9b15f6be6a3e80bbba1d
Anamicacodes <anamicagupta246@gmail.com>
2026-09-09 10:22:25 +0530

    Constructors

--- revision.cpp
+++ revision.cpp
@@ -1,18 +1,13 @@
- #include <bits/stdc++.h>
- using namespace std;
- int main(){
- for(int i=0; i<numsSize ; i++ ){
-        for(int j=0 ; j < numsSize; j ++){
-            int currentsum = arr[j]+ arr[i];
-            if (currentsum == target){
-                return i,j;
-            }
-            else if(currentsum > target){
-                j--;
-            }
-            else if (currentsum < target){
-                i++;
-            }
+#include <iostream>
+using namespace std;
+class Student{
+    public : 
+        int Roll;
+    Student(int x){
+            Roll = x;
         }
-    }
+    void operator(){
+        Roll++;      
+           Sufi.Roll++;
+    cout << Sufi.Roll;
 }
