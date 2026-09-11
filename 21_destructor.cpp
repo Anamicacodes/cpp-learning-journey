d603debcbb7c66935a1b7edf7ac31234c2495344
Anamicacodes <anamicagupta246@gmail.com>
2026-09-11 21:04:04 +0530

    Destructor code structure

---
+++ 21_destructor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Demo {
+    public : 
+        Demo(){
+        }
+        ~Demo(){
+        }
+};
+int main(){
+    Demo d1;
+    Demo d2;
+    return 0;
+}
