4850c2d73de26bb1edf7d45ec3bce85a5e902e68
Anamicacodes <anamicagupta246@gmail.com>
2026-09-14 10:24:09 +0530

    Default constrcutors

---
+++ 22_Default_constructor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Student {
+    int roll;
+    public :
+        Student(){
+            roll =10;
+            cout << roll << endl;
+        }
+};
+int main() {
+    Student s1,s2;
+}
