f7c762642667baeb4e257c830e37a6956394edc3
Anamicacodes <anamicagupta246@gmail.com>
2026-09-14 10:27:14 +0530

    Parameterized constructors

---
+++ 23_parameterized.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Student {
+    int roll;
+    public :
+        Student(int r){
+            roll =r;
+            cout << roll << endl;
+        }
+};
+int main(){
+    Student s1(101), s2(102);
+}
